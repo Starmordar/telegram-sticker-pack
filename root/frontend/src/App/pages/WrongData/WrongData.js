9534b63265e1105774425a79270da86872eaf1fa
RomanBorr <starmordar@gmail.com>
2019-08-08 18:06:22 +0300

    added header style for WrongData page

--- root/frontend/src/App/pages/WrongData/WrongData.js
+++ root/frontend/src/App/pages/WrongData/WrongData.js
@@ -24,28 +24,32 @@ class WrongData extends React.Component {
     }
 
     render() {
+        const SVG_BORDER_SIZE = 42;
         return (
             <section className="someth-wrong">
                 <div className="container h-100">
                     <div className='row justify-content-center align-items-center h-100'>
-                        <div className="gradient">
-                            <div className='test'
-                                style={{
-                                    height: this.state.height,
-                                    width: this.state.width
-                                }}></div>
-                            <div className='svg-container'>
 
-                                <SVGLaptop updateSize={this.updateSize} />
+                        <div className='svg-shadow'
+                            style={{
+                                height: this.state.height,
+                                width: this.state.width
+                            }}></div>
 
-                                <header className='header'>
-                                    <h2>Somethin' went wrong</h2>
-                                    <p>You trying to log in with wrong or outdated data, please try again</p>
-                                    <LoginButton />
-                                </header>
+                        <div className='svg-container'>
+                            <SVGLaptop updateSize={this.updateSize} />
 
-                            </div>
+                            <header className='header' style={{ width: this.state.width - SVG_BORDER_SIZE }}>
+                                <h2 className="heading font-weight-bold">WRONG DATA</h2>
+                                <p className="subheader">You trying to log-in with invalid or outdated data.
+                                 Make sure that you are using 
+                                 <a className='loginbot-link' href='tg://resolve?domain=testSamples_bot'> @login_bot </a> 
+                                  for sign-in and try again</p>
+                                <LoginButton />
+                            </header>
+
                         </div>
+
                     </div>
                 </div>
             </section>
