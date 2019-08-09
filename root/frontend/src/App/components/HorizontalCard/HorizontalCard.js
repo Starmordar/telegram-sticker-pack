379d1a3b8ef726e68ee630fb501a0e6ff58b912e
RomanBorr <starmordar@gmail.com>
2019-08-09 12:33:43 +0300

    clean up HorizontalCard component

--- root/frontend/src/App/components/HorizontalCard/HorizontalCard.js
+++ root/frontend/src/App/components/HorizontalCard/HorizontalCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './HorizontalCard.css'
 
+const iconStyle = {
+    position: 'absolute',
+    right: '20px',
+    top: '20px',
+    fontSize: '40px',
+    color: '#f07869'
+}
+
 class HorizontalCard extends React.Component {
     constructor(props) {
         super(props);
-
-        this.iconStyle = {
-            position: 'absolute',
-            right: '20px',
-            top: '20px',
-            fontSize: '40px',
-            color: '#f07869'
-        }
     }
 
     render() {
@@ -19,14 +19,14 @@ class HorizontalCard extends React.Component {
             <div className='card border-right-0 border-left-0 border-top-0'>
                 <div className='row no-gutters'>
 
-                    <div className="icon col-md-3">
-                        <i className={this.props.icon} aria-hidden="true" style={this.iconStyle}></i>
+                    <div className='icon col-md-3'>
+                        <i className={this.props.icon} aria-hidden='true' style={iconStyle}></i>
                     </div>
 
-                    <div className="text-content col-md-9">
-                        <div className="card-body">
-                            <h5 className="card-title font-weight-bold">{this.props.title}</h5>
-                            <p className="card-text">{this.props.text}</p>
+                    <div className='text-content col-md-9'>
+                        <div className='card-body'>
+                            <h5 className='card-title font-weight-bold'>{this.props.title}</h5>
+                            <p className='card-text'>{this.props.text}</p>
                         </div>
                     </div>
 
