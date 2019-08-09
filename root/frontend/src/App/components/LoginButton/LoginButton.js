df22f7716e7838ede24874a59704bcdbf76fcb23
RomanBorr <starmordar@gmail.com>
2019-08-09 12:26:18 +0300

    clean up LoginButton component

--- root/frontend/src/App/components/LoginButton/LoginButton.js
+++ root/frontend/src/App/components/LoginButton/LoginButton.js
@@ -2,24 +2,12 @@ import React from 'react';
 import './LoginButton.css'
 
 class LoginButton extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.btnContent = 'Login with Telegram';
-        this.href = 'tg://resolve?domain=testSamples_bot'
-    }
-
     render() {
         return (
-            <a href={this.href}
+            <a href='tg://resolve?domain=testSamples_bot'
                 className='btn btn-danger'
-                style={{
-                    backgroundColor: '#dc3545',
-                    color: '#ffffff',
-                    padding: '13px 40px',
-                    fontSize: '20px'
-                }}
-            >{this.btnContent}</a>
+                style={{ padding: '13px 40px', fontSize: '23px' }}>
+                Login with Telegram</a>
         )
     }
 }
