7bf438f008c47d8e0167d2ebbef0ee1eb31552ce
RomanBorr <starmordar@gmail.com>
2019-09-02 10:56:27 +0300

    add top navbar

--- root/frontend/src/App/pages/Main/Main.js
+++ root/frontend/src/App/pages/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Main.css';
 
 import NavSection from '../../components/NavSection/NavSection';
+import TopNavbar from '../../components/TopNavbar/TopNavbar';
 
 class Main extends React.Component {
     constructor(props) {
@@ -11,26 +12,33 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div className='left-side-navbar dark-blue'>
-                <div className='navbar-content'>
-                    <div className='brand-wrapper'>
-                        <a href='/' className='brand-name'>Sticker Squad</a>
-                    </div>
+            <>
+                <div className='left-side'>
+                    <div className='left-side-navbar dark-blue'>
+                        <div className='navbar-content'>
+                            <div className='brand-wrapper'>
+                                <a href='/' className='brand-name'>Sticker Squad</a>
+                            </div>
 
-                    <NavSection num={1} />
-                    <NavSection num={2} />
-                    <NavSection num={3} />
-                    <NavSection num={4} />
+                            <NavSection num={1} />
+                            <NavSection num={2} />
+                            <NavSection num={3} />
+                            <NavSection num={4} />
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div className='right-side'>
+                    <TopNavbar />
+                </div>
+            </>
         )
     }
 }
 
 const ExpandElement = (props) => {
     return (
-        <li className='custom-collapse__collapse-elements__element'>
-            <a href='javascript:void(0)' className='custom-collapse__collapse-elements__element__link'>{props.packName}</a>
+        <li className='custom-collapse__element'>
+            <a href='javascript:void(0)' className='custom-collapse__element__link'>{props.packName}</a>
         </li>
     )
 }
