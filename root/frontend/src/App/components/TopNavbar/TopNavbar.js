f48470fc375b885cb46e0bc34793e3a8556f0c4e
RomanBorr <starmordar@gmail.com>
2019-09-02 20:34:46 +0300

    done top navbar

--- root/frontend/src/App/components/TopNavbar/TopNavbar.js
+++ root/frontend/src/App/components/TopNavbar/TopNavbar.js
@@ -6,23 +6,51 @@ class TopNavbar extends React.Component {
     constructor(props) {
         super(props);
 
+        this.onFocus = this.onFocus.bind(this);
+        this.onFocusOut = this.onFocusOut.bind(this);
+    }
+
+    onFocus(e) {
+        const eventTrigger = e.currentTarget;
+        const pathOfSearchSvgIcon = eventTrigger.previousElementSibling.firstChild;
+
+        pathOfSearchSvgIcon.style.fill = 'blue';
+    }
+
+    onFocusOut(e) {
+        const eventTrigger = e.currentTarget;
+        const pathOfSearchSvgIcon = eventTrigger.previousElementSibling.firstChild;
+
+        pathOfSearchSvgIcon.style.fill = 'gray';
     }
 
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light justify-content-between" style={{ backgroundColor: '#ffffff' }}>
 
-                <form className="form-inline w-75 search-form flex-nowrap">
+                <form className="form-inline w-75 search-form flex-nowrap mr-sm-2 pl-sm-4 ml-sm-2">
 
-                    <svg aria-hidden="true" class="svg-icon" width="14" height="14" viewBox="0 0 18 18">
-                        <path d="M18 16.5l-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5zM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0z">
+                    <svg aria-hidden="true" className="svg-icon" width="13" height="13" viewBox="0 0 18 18">
+                        <path d="M18 16.5l-5.14-5.18h-.35a7 7 0 1 0-1.19 1.19v.35L16.5 18l1.5-1.5zM12 7A5 5 0 1 1 2 7a5 5 0 0 1 10 0z" fill={'gray'}>
                         </path>
                     </svg>
 
-                    <input className="form-control mr-sm-2 pl-sm-4 ml-sm-2 w-100 search-form__input" type="search" placeholder=" Search by sticker pack name" aria-label="Search" />
+                    <input className="form-control w-100 search-form__input ml-sm-2"
+                        type="search"
+                        tabIndex="0"
+                        placeholder=" Search by sticker pack name"
+                        aria-label="Search"
+                        onFocus={this.onFocus}
+                        onBlur={this.onFocusOut} />
                 </form>
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
@@ -30,7 +58,7 @@ class TopNavbar extends React.Component {
                     <ul className="navbar-nav">
 
                         <li className="nav-item mr-sm-2 ml-sm-2">
-                            <button type="button" class="btn btn-success create-button">Create</button>
+                            <button type="button" className="btn btn-success create-button">Create</button>
                         </li>
 
                         <li className="nav-item mr-sm-2">
@@ -49,7 +77,7 @@ class TopNavbar extends React.Component {
                         </li>
 
                         <li className="nav-item" >
-                            <a className='avatar-wrapper' href='/'>
+                            <a className='avatar-link' href='/'>
                                 <img src='https://images.alphacoders.com/914/914670.jpg'
                                     alt="User avatar" className='avatar' />
                             </a>
